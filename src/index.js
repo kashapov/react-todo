09453b3381b8c0ebff1ca4d8ad8e95d27141db3d
Andrey Kashapov <kashapovandrey@gmail.com>
2019-01-13 14:20:52 +0300

    add keys for TodoList

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import TodoList from "./components/TodoList";
 
 const App = () => {
   const todoData = [
-    { label: "Learn React", important: false },
-    { label: "Learn Redux", important: false },
-    { label: "Build Awesome App", important: true }
+    { id: 1, label: "Learn React", important: false },
+    { id: 2, label: "Learn Redux", important: false },
+    { id: 3, label: "Build Awesome App", important: true }
   ];
 
   return (
