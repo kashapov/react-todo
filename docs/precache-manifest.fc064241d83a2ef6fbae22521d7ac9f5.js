183f15540b388a5539e32d3eaa0c13406aefa5e7
Andrey Kashapov <kashapovandrey@gmail.com>
2019-09-19 15:39:40 +0300

    add /docs

---
+++ docs/precache-manifest.fc064241d83a2ef6fbae22521d7ac9f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f75e284f90f947ca9f302cb1640eb9",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "2d200cfebe8bfe1ca1d6",
+    "url": "/react-todo/static/css/main.51191f7d.chunk.css"
+  },
+  {
+    "revision": "24171593e979e9a7a8e9",
+    "url": "/react-todo/static/js/2.ec3fe930.chunk.js"
+  },
+  {
+    "revision": "2d200cfebe8bfe1ca1d6",
+    "url": "/react-todo/static/js/main.74557c96.chunk.js"
+  },
+  {
+    "revision": "31ba4ba914284ff60672",
+    "url": "/react-todo/static/js/runtime~main.0c4a585c.js"
+  }
+]);
